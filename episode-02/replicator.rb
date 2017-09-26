550a84a598980d2a37596b2af562a47b3587d0d2
Argir Argirov <dj.giro@gmail.com>
2017-09-26 14:53:52 -0400

    debugging

--- episode-02/replicator.rb
+++ episode-02/replicator.rb
@@ -1,3 +1,5 @@
+
+
 class Replicator
 
   # When the Enterprise calls Replicator.new, this method executes.
@@ -40,7 +42,7 @@ class Replicator
   # and then manually execute each method to ensure
   # it returns what's expect.
   def replicate(recipe)
-
+  
     # Setup an instance variable for the recipe
     # so that other methods can see what the recipe is
     @recipe = recipe
@@ -125,7 +127,7 @@ class Replicator
     # Abort if there is no glass inside the replicator or no power.
     return unless glass_inside_replicator && @power
 
-    mouse_chews_through_power_cord
+    # mouse_chews_through_power_cord
 
     # Draw power if possible and then mix ingredients around.
     if @enterprise.reactor.draw_power(3)
